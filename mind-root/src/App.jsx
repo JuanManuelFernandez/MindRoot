56d9600a7444734ca72a5f4325689451a6cdffdd
JuanManuelFernandez <juanmanuelfernandezbogo@gmail.com>
2026-07-16 23:17:07 -0300

    Primeras bases del diseño

---
+++ mind-root/src/App.jsx
@@ -0,0 +1,36 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <section class="SeccionMain">
+      <div class="MenuLateral">
+        <span class="TituloMenu">Mind Root</span>
+
+        <span class="TituloNotas">Notas</span>
+
+        <ul class="ListaNotas">
+          <li>Ideas</li>
+          <li>Tareas Diarias</li>
+          <li>Projectos</li>
+          <li>Recordatorios</li>
+
+        </ul>
+      </div>
+
+      <div class="Central">
+        <div class="ContenedorSuperior">
+          <input class="InputBuscar" placeholder='Buscar nota...'></input>
+          <button class="BtnAgregar">+</button>
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default App
